ec20c97791b0c2089b108fd8158dada9f548230c
sooraj2022skipq <109619318+sooraj2022skipq@users.noreply.github.com>
2022-09-23 05:59:49 +0500

    reverseString
    reverseString

---
+++ day_4/ReverseInterString.java
@@ -0,0 +1,34 @@
+import java.util.*; 
+import java.io.*;
+
+class Main {
+
+  public static String StringChallenge(String str) {
+    // code goes here
+    char[] arr = str.toCharArray();
+
+    String reversString = "";
+    String interString = "0r5ftupeda";
+    int index = 0;
+    for(int i = str.length() - 1 ; i >= 0; i--){
+        
+        reversString += arr[i];
+        
+        if( index < interString.length()){
+          reversString += interString.charAt(index);
+          index++;
+        }
+         
+    }
+    if(index < interString.length()){
+      reversString += interString.substring(index, interString.length());
+    }
+    return reversString;
+  }
+
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+
+    System.out.print(StringChallenge(s.nextLine())); 
+  }
